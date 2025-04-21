e79c7f4c938301b454480b14db524afed4a254f3
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-21 17:55:58 +0300

    feat: add Partners component

---
+++ direct-help/src/modules/Partners/Partners.tsx
@@ -0,0 +1,23 @@
+import Button from "@/components/Button/Button";
+import { PARTNERS_DATA } from "./data";
+import Image from "next/image";
+
+const Partners = () => {
+  return (
+    <div className="flex flex-col gap-[65px]">
+      <section className="mt-[72px] pb-[85px] px-20 bg-gradient-custom flex flex-col gap-[49px]">
+        <h2 className="font-family text-[40px] leading-[47px] text-text2">Наші партнери</h2>
+        <div className="flex gap-[93px] justify-around">
+          {PARTNERS_DATA && PARTNERS_DATA.map(({ alt, id, img }) => (
+            <Image alt={alt} src={img} key={id} width={250} height={200} />
+          ))}
+        </div>
+      </section>
+      <div className="w-[420px] self-end mr-20">
+        <Button size="full" variant="second" text="Стати партнером" />
+      </div>
+    </div>
+  )
+}
+
+export default Partners;
---
+++ direct-help/src/modules/Partners/data.ts
@@ -0,0 +1,6 @@
+export const PARTNERS_DATA = [
+  { id: 1, img: '/partners/partner4.png', alt: '' },
+  { id: 2, img: '/partners/partner2.png', alt: '' },
+  { id: 3, img: '/partners/partner3.png', alt: '' },
+  { id: 4, img: '/partners/partner1.png', alt: '' },
+];

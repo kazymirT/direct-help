76480baf44d010e4d2617a8a37103c0ccc8bf418
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-26 14:21:26 +0300

    refactor: update about page

--- direct-help/src/app/about/page.tsx
+++ direct-help/src/app/about/page.tsx
@@ -1,9 +1,13 @@
-import PresentationPage from "@/modules/Presentation/Presentation";
+// import PresentationPage from "@/modules/Presentation/Presentation";
+
+import Image from "next/image";
 
 export default function About() {
     return (
       <section>
-        <PresentationPage />
+        {Array.from({length: 11}).map((_, index) => (
+          <Image key={index} src={`/about/about_${index}.png`} alt="" width={1440} height={810} className="w-full h-auto" />
+        ))}
       </section>
     );
   }

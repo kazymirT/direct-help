94a1cd4418be3be1c95f66658cae1f97fadc0394
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-26 12:23:37 +0300

    feat: add about page

--- direct-help/src/app/about/page.tsx
+++ direct-help/src/app/about/page.tsx
@@ -1,8 +1,10 @@
+import PresentationPage from "@/modules/Presentation/Presentation";
+
 export default function About() {
     return (
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <h1>Про нас</h1>
-      </div>
+      <section>
+        <PresentationPage />
+      </section>
     );
   }
   

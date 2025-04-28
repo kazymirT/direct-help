b968570bcc3acb4ac2531160a477d33fae313197
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-04-28 21:14:11 +0300

    feat: add LocaleSwitcherButtons

---
+++ direct-help/src/components/LocaleSwitcher/LocaleButton.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import {useParams} from 'next/navigation';
+import {FC, useTransition} from 'react';
+import {usePathname, useRouter} from '@/i18n/navigation';
+import { useLocale } from 'next-intl';
+import clsx from 'clsx';
+
+export interface LocaleSwitcherProps {
+  local: string;
+  name: string;
+}
+
+const LocaleButton: FC<LocaleSwitcherProps> = ({ local, name}) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const params = useParams();
+  const locale = useLocale();
+
+  const onChangeLocal = () => {
+    startTransition(() => {
+      router.replace(
+        // @ts-expect-error -- TypeScript will validate that only known `params`
+        {pathname, params},
+        {locale: local}
+      );
+    });
+  }
+  const buttonCN = clsx('text-lg uppercase cursor-pointer leading-1 disabled:cursor-auto', {
+    ['text-[#2E2C2C]']: locale !== local,
+    ['text-[#1B1A1A]']: locale === local,
+  })
+  return (
+    <button 
+      disabled={isPending || local === locale}
+      onClick={onChangeLocal}
+      className={buttonCN}>
+      {name}
+    </button>
+  );
+}
+
+export default LocaleButton;
---
+++ direct-help/src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -0,0 +1,18 @@
+import { LOCAL_LINKS } from '@/modules/Header/data';
+import LocaleButton from './LocaleButton';
+import { Fragment } from 'react';
+
+const LocaleSwitcher = () => {
+  return (
+    <nav className='flex gap-1 h-fit items-center'>
+      {LOCAL_LINKS.map(({ id, ...props }) => (
+        <Fragment key={id}>
+        {id > 1 && <span className='text-[#2E2C2C] text-3xl leading-6'>|</span>}
+        <LocaleButton {...props} />
+        </Fragment>
+      ))}
+    </nav>
+  );
+}
+
+export default LocaleSwitcher;

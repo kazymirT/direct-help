5c2222e8085445dea0c3a6aeea38d411c9b3a9be
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-01 16:59:12 +0300

    fix: delete files

--- direct-help/src/app/[locale]/new-partner/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewPartner from "@/modules/Forms/NewPartner/NewPartner";
-
-export default function Form() {
-  return (
-    <section className="flex flex-col m-auto w-full max-w-[1280px]">
-        <NewPartner />
-    </section>
-  );
-}

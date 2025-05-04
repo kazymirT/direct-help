147f5570fc04cfced57e10ee00e00785a6b3587f
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-04 10:47:49 +0300

    feat: add responsive styles for Reporting page

--- direct-help/src/app/[locale]/(main)/reporting/page.tsx
+++ direct-help/src/app/[locale]/(main)/reporting/page.tsx
@@ -3,7 +3,7 @@ import ReportingList from "@/modules/ReportingList/ReportingList";
 
 export default function Reporting() {
   return (
-    <div className="flex flex-col items-center gap-18 pb-50">
+    <div className="flex flex-col items-center gap-3 xs:gap-6 sm:gap-8 md:gap-9 lg:gap-7 xl:gap-18 pb-14 xs:pb-20 sm:pb-30 md:pb-40 lg:pb-45 xl:pb-50">
       <ReportingBanner />
       <ReportingList />
     </div>
--- direct-help/src/modules/ReportingList/ReportingList.tsx
+++ direct-help/src/modules/ReportingList/ReportingList.tsx
@@ -9,14 +9,14 @@ const ReportingList = async () => {
 
   const isUK = locale === 'uk';
 
-  const descriptionCN = clsx("text-4xl text-[#2f2d2d] w-full leading-[1.2]", {
-    ['max-w-[852px]']: isUK,
-    ['max-w-[1063px]']: !isUK
+  const descriptionCN = clsx("text-[clamp(1rem,0.567rem+1.849vw,1.75rem)] lg:text-[clamp(1.75rem,0.519rem+1.923vw,2.25rem)] text-[#2f2d2d] w-full leading-[1.2]", {
+    ['max-w-[328px] xs:max-w-[400px] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[680px] xl:max-w-[852px]']: isUK,
+    ['max-w-[328px] xs:max-w-[400px] sm:max-w-[600px] md:max-w-[700px] lg:max-w-[680px] xl:max-w-[852px] max-w-[1063px]']: !isUK
   })
   return (
-    <section className="w-full max-w-[1280px] flex flex-col gap-17">
+    <section className="wrapper flex flex-col gap-2 xs:gap-[10px] sm:gap-3 md:gap-[15px] lg:gap-17">
       <h2 className={descriptionCN}>{t('description')}</h2>
-      <div className="flex flex-wrap gap-x-[20px] gap-y-[64px]">
+      <div className="reporting-section">
         {REPORTING_DATA.map((data) => (
           <ReportingCard key={data.id} {...data}  />        
         ))}

3bc990a3a0099bd009dde798930f5ac5c51ad24d
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-06 14:59:51 +0300

    feat: add responsive styles for Partners and About modules

--- direct-help/src/modules/About/About.tsx
+++ direct-help/src/modules/About/About.tsx
@@ -11,44 +11,44 @@ const About = async () => {
   const isUK = locale === 'uk';
   const isDE = locale === 'de';
 
-  const wrapperCN = clsx('w-full max-w-[1280px] flex flex-col items-end', {
-    ['gap-[212px]']: isEN,
-    ['gap-[190px]']: isUK,
-    ['gap-[196px]']: isDE,
+  const wrapperCN = clsx('wrapperAbout flex flex-col items-end', {
+    ['gap-14 md:gap-25 lg:gap-[140px] xl:gap-[212px]']: isEN,
+    ['gap-14 md:gap-25 lg:gap-[146px] xl:gap-[190px]']: isUK,
+    ['gap-14 md:gap-25 lg:gap-[140px] xl:gap-[196px]']: isDE,
   });
-  const descriptionsCN = clsx('w-full max-w-[522px] text-[28px] text-text2', {
+  const descriptionsCN = clsx('w-full max-sm:max-w-[clamp(18.063rem,-7.675rem+109.811vw,36.25rem)] max-[850px]:max-w-9/10 min-[850px]:max-w-[40%] lg:max-w-[clamp(26.688rem,12.072rem+22.837vw,32.625rem)] text-[clamp(1rem,0.819rem+0.77vw,1.313rem)] lg:text-[clamp(1.313rem,0.236rem+1.683vw,1.75rem)] text-text2', {
     ['pt-[10px] -tracking-[0.0em] leading-[1.17]']: isEN,
-    ['pt-[34px] leading-[1.2]']: isUK,
+    ['lg:pt-0 xl:pt-[34px] leading-[1.2]']: isUK,
     ['leading-[1.175]']: isDE,
   });
   const containerCN = clsx('w-full flex justify-center', {
-    ['pb-[84px] pt-[105px]']: isEN,
-    ['pb-[94px] pt-[90px]']: isUK,
-    ['pb-[97px] pt-[90px]']: isDE,
+    ['pb-12.5 md:pb-15 lg:pb-[84px] pt-4 xl:pt-[105px]']: isEN,
+    ['pb-12.5 md:pb-15 lg:pb-[94px] pt-4 xl:pt-[90px]']: isUK,
+    ['pb-12.5 md:pb-15 lg:pb-[97px] pt-4 xl:pt-[90px]']: isDE,
   });
 
   return (
     <div className={containerCN}>
       <div className={wrapperCN}>
-        <section className="w-full flex flex-col gap-4">
-        <h2 className="font-medium text-[36px] text-text2">{t('title')}</h2>
-        <div className="flex w-full justify-between">
+        <section className=" w-full flex flex-col gap-1.5 lg:gap-2 xl:gap-4">
+        <h2 className="font-medium text-[clamp(1.5rem,1.236rem+1.127vw,2.25rem)] text-text2">{t('title')}</h2>
+        <div className="flex w-full flex-col min-[850px]:flex-row justify-between gap-10 min-[850px]:gap-15 xl:gap-20">
         <div className={descriptionsCN}>
           <p>{t('description-one')}</p>
           <p className="leading-[1.15]">{t('description-two')}</p>
           <p>{t('description-three')}</p>
         </div>
-        <div className="relative pt-[96px] h-fit">
-        <Image alt="" src='/about.png' width={666} height={507} className="relative z-20 h-[507px]"  />
-          <div className="absolute top-[47px] -left-[58px] bg-[#0d1415] w-[237px] h-[227px] z-10"></div>
-          <div className="absolute top-1 right-0 bg-[#f8b428] w-[322px] h-[297px]"></div>
-          <div className="absolute -bottom-[92px] right-0 bg-[#0d1415] w-[363px] h-[95px]"></div>
-          <Image alt="" src="/icons/about.svg" width={323} height={263} className="absolute -bottom-11 -left-12 z-20" />
+        <div className="relative pt-5 md:pt-10 lg:pt-[70px] xl:pt-[96px] h-fit max-[850px]:w-fit max-[850px]:self-center">
+        <Image alt="" src='/about.png' width={301} height={212} className="relative w-[clamp(18.813rem,12.348rem+27.581vw,30rem)] lg:w-[clamp(30.625rem,3.394rem+42.548vw,41.688rem)] z-20 aspect-[1.31]"  />
+          <div className="absolute top-0 md:top-4 lg:top-8 xl:top-[47px] -left-8 lg:-left-11.5 xl:-left-[58px] w-[clamp(6.563rem,4.179rem+10.169vw,10.688rem)] lg:w-[clamp(10.688rem,0.534rem+15.865vw,14.813rem)] aspect-[1.04] z-10 bg-[#0d1415]"></div>
+          <div className="absolute bg-[#f8b428] aspect-[1.08] w-[clamp(9.125rem,5.803rem+14.176vw,14.875rem)] lg:w-[clamp(14.813rem,1.736rem+20.433vw,20.125rem)] -top-6 md:top-0 xl:top-1 right-0"></div>
+          <div className="absolute bg-[#0d1415] aspect-[3.82] w-[clamp(10.125rem,6.514rem+15.408vw,16.375rem)] lg:w-[clamp(16.438rem,1.053rem+24.038vw,22.688rem)] -bottom-11 md:-bottom-[60px] xl:-bottom-[92px] right-0"></div>
+          <Image alt="" src="/icons/about.svg" width={143} height={108} className="absolute aspect-[1.32] z-20 w-[clamp(8.938rem,5.687rem+13.867vw,14.563rem)] lg:w-[clamp(14.563rem,1.639rem+20.192vw,19.813rem)] -bottom-5 md:-bottom-6.5 xl:-bottom-11 -left-6 md:-left-12" />
         </div>
         </div>
         </section>
-        <div className="w-full max-w-[1290px] self-center flex justify-end">
-          <div className="w-full max-w-[416px]">
+        <div className="w-full self-center flex justify-end">
+          <div className="w-full max-w-[330px] min-xs:max-w-[241px] lg:max-w-[341px] xl:max-w-[416px] lg:pr-[clamp(3.125rem,0.356rem+4.327vw,4.25rem)] xl:pr-0">
           <Button size="full" variant="second" text={t('button')} link="/about" />
           </div>
         </div>
--- direct-help/src/modules/Partners/Partners.tsx
+++ direct-help/src/modules/Partners/Partners.tsx
@@ -8,17 +8,17 @@ const OPTIONS: EmblaOptionsType = { loop: true, align: 'start', duration: 500, s
 const Partners = async () => {
   const t = await getTranslations('home.partners');
   return (
-    <div className="w-full mt-[72px] flex flex-col items-end gap-[65px]">
-      <div className="w-full bg-gradient-partners flex flex-col items-end pb-[92px]">
-      <section className="w-full max-w-[1280px] m-auto pb-[30px]">
-        <h2 className="font-family text-[36px] leading-[47px] text-text2">{t('title')}</h2>
+    <div className="w-full lg:mt-[72px] flex flex-col items-end md:gap-5 lg:gap-10 xl:gap-[65px]">
+      <div className="w-full bg-gradient-partners flex flex-col items-end pb-5 xs:pb-7 sm:pb-9 md:pb-[92px]">
+      <section className="wrapper m-auto pb-3 sm:pt-4 md:pt-5 lg:pt-0 lg:pb-[30px]">
+        <h2 className="font-family text-[clamp(1.563rem,1.32rem+1.033vw,2.25rem)] leading-[2] lg:leading-[1.3] text-text2">{t('title')}</h2>
       </section>
       <div className="w-full">
         <Slider slides={PARTNERS_DATA} options={OPTIONS}  />
       </div>
       </div>
-      <div className="w-full max-w-[1280px] flex justify-end m-auto">
-      <div className="w-[415px]">
+      <div className="max-md:hidden wrapper flex justify-end m-auto">
+      <div className="w-[241px] lg:w-[350px] xl:w-[415px]">
         <Button size="full" variant="second" text={t('button')} link="/new-partner" />
       </div>
       </div>

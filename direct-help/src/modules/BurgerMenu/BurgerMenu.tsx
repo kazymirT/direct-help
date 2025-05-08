878d6bb7c9a0bf178a2885cd0c463d5188150666
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-05-08 11:31:35 +0300

    feat: add burger menu

---
+++ direct-help/src/modules/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import Menu from './Menu';
+
+export default function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const closeMenu = () => setIsOpen(false);
+  const openMenu = () => setIsOpen(true);
+ 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isOpen]);
+  
+  return (
+    <div className="relative z-50 min-lg:hidden">
+      <button onClick={openMenu} className="relative">
+        <Image src='/burger.svg' alt="menu open icon" width={24} height={17} />
+      </button>
+      <Menu closeMenu={closeMenu} isOpen={isOpen} menuRef={menuRef} />
+      {isOpen && (
+        <div onClick={closeMenu} className="fixed top-0 left-00 w-full h-[100dvh] inset-0 bg-black/40 z-30 transition-opacity duration-300" />
+      )}
+    </div>
+  );
+}
--- direct-help/src/modules/Header/Header.tsx
+++ direct-help/src/modules/Header/Header.tsx
@@ -3,14 +3,16 @@ import { HEADER_LINKS } from "./data";
 import LocaleSwitcher from "@/components/LocaleSwitcher/LocaleSwitcher";
 import { getTranslations } from "next-intl/server";
 import { Link } from "@/i18n/navigation";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 const Header = async () => {
   const t = await getTranslations('header');
 
   return (
-    <header className="w-full relative pt-[7px] pb-[11px] xs:pt-3 xs:pb-3 sm:pt-4 sm:pb-4 md:pt-5 md:pb-5 lg:pt-[26px] lg:pb-[48px] min-xl:pt-[32px] min-xl:pb-8 flex justify-center">
+    <header className="w-full z-100 relative pt-[7px] pb-[11px] xs:pt-3 xs:pb-3 sm:pt-4 sm:pb-4 md:pt-5 md:pb-5 lg:pt-[26px] lg:pb-[48px] min-xl:pt-[32px] min-xl:pb-8 flex justify-center">
           <div className="m-auto w-full flex justify-between max-lg:items-center">
-          <Image src='/burger.svg' alt="menu open icon" width={24} height={17} className="min-lg:hidden" />
+          {/* <Image src='/burger.svg' alt="menu open icon" width={24} height={17} className="min-lg:hidden" /> */}
+          <BurgerMenu/>
           <Link href={'/'} className="max-lg:absolute max-lg:left-1/2 max-lg:-translate-x-1/2 max-lg:top-1/2 max-lg:-translate-y-1/2">
             <Image src='/logo.svg' alt="logo" width={71} height={30} className="w-[clamp(4.438rem,3.426rem+4.314vw,6.188rem)] lg:w-[clamp(6.188rem,0.034rem+9.615vw,8.688rem)] aspect-[2.37] min-2xl:w-[139px]" />
           </Link>
